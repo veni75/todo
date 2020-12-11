e0f389d3eea03fc1a509512182793ac91dc5b72b
Várdai Enikő <vardai.eniko@gmail.com>
2020-12-11 12:55:04 +0100

    fv igaazitas

--- todo.js
+++ todo.js
@@ -2,16 +2,19 @@
 
 const date = document.querySelector('.date');
 const day = document.querySelector('.day');
-const todoList = document.querySelector('.todo__list');
-const todoDoneList = document.querySelector('.todo__doneList');
-const clear = document.querySelector('.todo__clear');
 const input = document.querySelector('input');
 const counter = document.querySelector('.todo__counter');
 const doneCounter = document.querySelector('.todo__doneCounter');
-const hide = document.querySelector('.todo__hide');
+const todoList = document.querySelector('.todo__list');
+const todoDoneList = document.querySelector('.todo__doneList');
+const clear = document.querySelector('.todo__clear');
 const complete = document.querySelector('.todo__complete');
+const hide = document.querySelector('.todo__hide');
 let todoNone = document.querySelector('.todo__none');
-let todoElement, todoWhite, todoRed, todoDiv;
+const button = document.querySelector('button');
+
+let todoElement, todoWhite, todoRed, todoDiv, todoElementLabel;
+let todoDoneDiv, todoElementDone, todoElementLabelDone;
 let inputText;
 let number = 0;
 let sorszam = 0;
@@ -24,7 +27,6 @@ const dateHandler = () => {
     day.textContent = new Intl.DateTimeFormat('en-GB', options).format(now);
     date.textContent = new Intl.DateTimeFormat('en-GB').format(now);
 }
-dateHandler();
 
 const todoDoneHide = () => {
     hide.addEventListener('click', () => {
@@ -32,7 +34,15 @@ const todoDoneHide = () => {
         hide.textContent = hide.textContent.includes('Hide') ? 'Show Complete' : 'Hide Complete';
     })
 }
-todoDoneHide();
+
+const numberSet = (value) => {
+    const numberSet1 = {
+        0: () => number = 0,
+        1: () => number += 1,
+        2: () => number -= 1,
+    }
+    counter.textContent = numberSet1.value;
+}
 
 const clearTodo = () => {
     let i = todoList.childNodes.length;
@@ -46,26 +56,8 @@ const clearTodo = () => {
     todoNone.classList.remove('hide');
 }
 
-/* const numberSet1 = {
-    0: ()=>number =0,
-    1: ()=>number +=1,
-    2: ()=>number -=1,
-} */
-
-const numberSet = (value) => {
-    if (value === 1) {
-        number += 1;
-    } else if (value === -1) {
-        number -= 1;
-    } else if (value === 0) {
-        number = 0;
-    }
-    counter.textContent = number;
-}
-
 const deleteTodo = (ev) => {
-    const elem = ev.currentTarget;
-    const todoDiv = elem.parentElement;
+    const todoDiv = ev.currentTarget.parentElement;
     const todoId = todoDiv.className.split(' ').pop();
     todoDiv.parentElement.removeChild(todoDiv);
     localStorage.removeItem(`todo${todoId}`);
@@ -75,14 +67,33 @@ const deleteTodo = (ev) => {
     }
 }
 
-const redElement = (element) => {
+const createRedElement = (element) => {
     todoRed = document.createElement('div');
     element.appendChild(todoRed);
     todoRed.className = ('hide');
     const redIcon = document.createElement('i');
     todoRed.appendChild(redIcon);
-    redIcon.className = ('fa fa-bitbucket');
+    redIcon.className = ('fa fa-trash');
     todoRed.addEventListener('click', deleteTodo);
+}
+
+const createTodoDiv = (sorszam) => {
+    todoDiv = document.createElement('div');
+    todoDiv.className = (`todoDiv todo${sorszam}`);
+    todoElement = document.createElement('input');
+    todoElement.setAttribute('type', 'checkbox');
+    todoElement.setAttribute('id', `todo${sorszam}`);
+    todoElement.setAttribute('name', `todo${sorszam}`);
+    todoElementLabel = document.createElement('label');
+    todoElementLabel.setAttribute('for', `todo${sorszam}`);
+    todoElementLabel.innerHTML = inputText;
+    todoList.appendChild(todoDiv);
+    todoDiv.appendChild(todoElement);
+    todoDiv.appendChild(todoElementLabel);
+    createRedElement(todoDiv);
+    todoDiv.addEventListener('mouseover', ev => ev.currentTarget.lastChild.className = ('appearRed'));
+    todoDiv.addEventListener('mouseleave', ev => ev.currentTarget.lastChild.className = ('hide'));
+    todoElement.addEventListener('click', checkBox);
 }
 
 const toDo = () => {
@@ -91,48 +102,32 @@ const toDo = () => {
     sorszam += 1;
     localStorage.setItem(`todo${sorszam}`, inputText);
     input.value = '';
-    todoDiv = document.createElement('div');
-    todoDiv.className = (`todoDiv todo${sorszam}`);
-    todoElement = document.createElement('input');
-    todoElement.setAttribute('type', 'checkbox');
-    todoElement.setAttribute('id', `todo${sorszam}`);
-    todoElement.setAttribute('name', `todo${sorszam}`);
-    let todoElementLabel = document.createElement('label');
-    todoElementLabel.setAttribute('for', `todo${sorszam}`);
-    todoElementLabel.innerHTML = inputText;
-    todoList.appendChild(todoDiv);
-    todoDiv.appendChild(todoElement);
-    todoDiv.appendChild(todoElementLabel);
-    redElement(todoDiv);
-    todoDiv.addEventListener('mouseover', ev => ev.currentTarget.lastChild.className = ('appearRed'));
-    todoDiv.addEventListener('mouseleave', ev => ev.currentTarget.lastChild.className = ('hide'));
-    todoElement.addEventListener('click', checkBox);
-    console.log(todoNone.classList);
-    if (!todoNone.classList.contains('hide')){
+    createTodoDiv(sorszam);
+    if (!todoNone.classList.contains('hide')) {
         todoNone.classList.add('hide');
     }
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', toDo);
-clear.addEventListener('click', clearTodo);
+const createTodoDoneDiv = (ev) => {
+    todoDoneDiv = document.createElement('div');
+    todoDoneDiv.className = ('todoDoneDiv');
+    todoElementDone = document.createElement('input');
+    todoElementDone.setAttribute('type', 'checkbox');
+    todoElementDone.setAttribute('id', `todoDone${sorszam}`);
+    todoElementDone.setAttribute('name', `todoDone${sorszam}`);
+    todoElementDone.checked = true;
+    todoElementLabelDone = document.createElement('label');
+    todoElementLabelDone.setAttribute('for', `todo${sorszam}`);
+    todoElementLabelDone.innerHTML = ev.currentTarget.parentElement.textContent;
+    todoDoneList.appendChild(todoDoneDiv);
+    todoDoneDiv.appendChild(todoElementDone);
+    todoDoneDiv.appendChild(todoElementLabelDone);
+}
 
 const checkBox = (ev) => {
     if (ev.currentTarget.checked === true) {
         ev.currentTarget.parentElement.parentElement.removeChild(ev.currentTarget.parentElement);
-        const todoDoneDiv = document.createElement('div');
-        todoDoneDiv.className = ('todoDoneDiv');
-        const todoElementDone = document.createElement('input');
-        todoElementDone.setAttribute('type', 'checkbox');
-        todoElementDone.setAttribute('id', `todoDone${sorszam}`);
-        todoElementDone.setAttribute('name', `todoDone${sorszam}`);
-        todoElementDone.checked = true;
-        let todoElementLabelDone = document.createElement('label');
-        todoElementLabelDone.setAttribute('for', `todo${sorszam}`);
-        todoElementLabelDone.innerHTML = ev.currentTarget.parentElement.textContent;
-        todoDoneList.appendChild(todoDoneDiv);
-        todoDoneDiv.appendChild(todoElementDone);
-        todoDoneDiv.appendChild(todoElementLabelDone);
+        createTodoDoneDiv(ev);
         done += 1;
         percent = done / sorszam;
         numberSet(-1);
@@ -140,36 +135,29 @@ const checkBox = (ev) => {
     }
 }
 
+const listenerSet = () => {
+    button.addEventListener('click', toDo);
+    clear.addEventListener('click', clearTodo);
+}
+
 const toDoStart = (i) => {
-    todoDiv = document.createElement('div');
-    todoDiv.className = (`todoDiv todo${i}`);
-    todoElement = document.createElement('input');
-    todoElement.setAttribute('type', 'checkbox');
-    todoElement.setAttribute('id', `todo${i}`);
-    todoElement.setAttribute('name', `todo${i}`);
-    let todoElementLabel = document.createElement('label');
-    todoElementLabel.setAttribute('for', `todo${i}`);
-    todoList.appendChild(todoDiv);
-    todoDiv.appendChild(todoElement);
-    todoDiv.appendChild(todoElementLabel);
-    redElement(todoDiv);
-    todoDiv.addEventListener('mouseover', ev => ev.currentTarget.lastChild.className = ('appearRed'));
-    todoDiv.addEventListener('mouseleave', ev => ev.currentTarget.lastChild.className = ('hide'));
-    todoElement.addEventListener('click', checkBox);
+    createTodoDiv(i);
     todoElementLabel.textContent = localStorage.getItem(localStorage.key(i));
 }
 
 const start = () => {
+    dateHandler();
+    todoDoneHide();
     if (localStorage.length === 0) {
         todoNone.classList.remove('hide');
     } else {
         todoNone.classList.add('hide');
     }
-    for (let i = 0; i < localStorage.length; i++) {
-        toDoStart(i);
-    }
+    Object.keys(localStorage).forEach((element, index) => toDoStart(index));
+
     number = localStorage.length;
     counter.textContent = number;
     sorszam = number;
+    listenerSet();
 }
 start();
